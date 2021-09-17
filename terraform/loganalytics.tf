acbb8d31365ac2e90e9486721c0199963b400805
Mads Lilbæk <madslilbaek@gmail.com>
2021-09-17 16:13:14 +0200

    Fixed bad references for Log Analytics workspace

--- terraform/loganalytics.tf
+++ terraform/loganalytics.tf
@@ -5,8 +5,8 @@ resource "azurerm_resource_group" "loganalytics_rg" {
 
 resource "azurerm_log_analytics_workspace" "loganalytics_sandbox" {
   name                = "sandbox-la-1"
-  location            = azurerm_resource_group.example.location
-  resource_group_name = azurerm_resource_group.example.name
+  location            = azurerm_resource_group.loganalytics_rg.location
+  resource_group_name = azurerm_resource_group.loganalytics_rg.name
   sku                 = "PerGB2018"
   retention_in_days   = 30
 }
